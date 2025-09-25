4ee836abf7523e53942a2f5a76473818a11a3c18
Anamika Rai <raikhushi49738@gmail.com>
2025-09-25 18:56:09 +0530

    initial commit

---
+++ Java_String_Practices/Longest_Word.java
@@ -0,0 +1,15 @@
+package string_problems;
+import java.util.*;
+public class Longest_Word {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		str = str.trim();
+		int max = Integer.MIN_VALUE;
+		int count = 0;
+		
+		
+	}
+
+}
